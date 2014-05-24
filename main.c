d04620e6f9a178234f7dc883e9ac46e3423a4519
giovybus <giovanni.buscarino@gmail.com>
2014-05-24 17:57:15 +0200

    secondo

--- main.c
+++ main.c
@@ -3,4 +3,11 @@ asm("jmpl $0x0000, $main\n");
 
 void main(){
 
+	__asm__ __volatile__ ("movb $'G' ,%al\n");
+	__asm__ __volatile__ ("movb $0x0e, %ah\n");
+	__asm__ __volatile__ ("int $0x10\n");
+
+	__asm__ __volatile__ ("movb $'i', %al\n");
+	__asm__ __volatile__ ("movb $0x0e, %ah\n");
+	__asm__ __volatile__ ("int $0x10\n");
 }
